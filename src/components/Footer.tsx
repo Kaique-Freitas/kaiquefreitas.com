44796fa1510e7ecd27db99a04c832a020d0613a6
Kaique Freitas <kaiquefreitas@outlook.pt>
2022-05-07 17:34:35 -0300

    feat: adjust layout of footer when large content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,19 @@
-export function Footer() {
+interface FooterProps {
+  absolute?: boolean
+}
+
+export function Footer({ absolute }: FooterProps) {
   return (
-    <footer className='w-full py-3 phone:py-6 absolute bottom-0 border-t-[1px]'>
-      <a href='https://kaiquefreitas.com' rel='noopener noreferrer' className='flex justify-center phone:text-lg'>
+    <footer
+      className={`w-full py-3 phone:py-6 ${
+        absolute ? 'absolute' : 'mt-20'
+      } bottom-0 border-t-[1px]`}
+    >
+      <a
+        href='https://kaiquefreitas.com'
+        rel='noopener noreferrer'
+        className='flex justify-center phone:text-lg'
+      >
         {new Date().getFullYear()} @ Kaique Freitas
       </a>
     </footer>
