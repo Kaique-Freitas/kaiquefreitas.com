4f8403bb5d2458f4d2d7548530669da026d5a070
Kaique Freitas <kaiquefreitas@outlook.pt>
2022-10-04 12:52:14 -0300

    feat: define meta tag with og image

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -6,16 +6,18 @@ interface HeaderProps {
 }
 
 export function HeadComponent({ title, description }: HeaderProps) {
+  const faviconPath = 'favicon.ico'
+
   return (
     <Head>
       <title>{title}</title>
-      <link rel='icon' sizes='32x32' href='favicon.ico' />
-      <link rel='shortcut icon' sizes='32x32' href='favicon.ico' />
+      <link rel='icon' sizes='32x32' href={faviconPath} />
+      <link rel='shortcut icon' sizes='32x32' href={faviconPath} />
+      <meta property='og:image' content={faviconPath}/>
       <meta name='description' content={description} />
       <meta name='keywords' content='Kaique, Kaique Freitas, Kaique Freitas Dev' />
       <meta name='author' content='Kaique Freitas' />
       <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-      <link rel='icon' href='/favicon.ico' />
     </Head>
   )
 }
